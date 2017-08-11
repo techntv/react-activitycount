b2b6c295df3459754a601f5e89d44d0c91701c33
The Vinh <nguyenthevinh@vienthonga.com>
2017-08-11 13:34:02 +0700

    add react router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import SkiDayList from './components/SkiDayList.jsx';
 // import SkiDayCount from './components/SkiDayCount.jsx';
 import App from './components/App.jsx';
-
+import { Router, Route, hashHistory } from 'react-router';
 
 import jQuery from 'jquery';
 global.jQuery = jQuery;
@@ -14,9 +14,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'font-awesome/css/font-awesome.min.css';
 window.React = React;
-ReactDOM.render(<App />, 
-            document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // ReactDOM.render(<SkiDayCount />, 
 //             document.getElementById('app')
