ab4da43a4063830330aeaa98b010f5b4f6da7abf
The Vinh <nguyenthevinh@vienthonga.com>
2017-08-18 10:52:21 +0700

    Optimize code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import Member from './components/Member.jsx';
+
 import jQuery from 'jquery';
 global.jQuery = jQuery;
 global.$ = jQuery;
