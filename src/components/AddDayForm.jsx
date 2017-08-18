4d5e08edfff188d42d0e632a8da2e1fb2bfb71cb
The Vinh <nguyenthevinh@vienthonga.com>
2017-08-18 10:31:04 +0700

    Add atuocomplete form

--- src/components/AddDayForm.jsx
+++ src/components/AddDayForm.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+const tahoeResorts = [
+    "Alpine Meadows",
+    "Boreal",
+    "Diamond Peak",
+    "Donner Ski Ranch",
+    "Heavenly",
+    "Homewood",
+    "Kirkwood",
+    "Mt. Rose",
+    "Northstar",
+    "Squaw Valley",
+    "Sugar Bowl"
+]
+
+class Autocomplete extends Component {
+    get value(){
+        return this.refs.inputResort.value;
+    }
+    set value(inputValue){
+        this.refs.inputResort.value = inputValue;
+    }
+    render(){
+        return(
+            <div>
+                <input ref="inputResort" type="text" list="tahoe-resorts" />
+                <datalist id="tahoe-resorts">
+                    {
+                        this.props.options.map(
+                            (opt,i) => 
+                            <option key={i}>{opt}</option>
+                            )
+                    }
+                </datalist>
+            </div>
+        )
+    }
+}
 const AddDayForm = ({resort, date, powder, backcountry, onNewDay}) => {
      const submit = (event) => {
             event.preventDefault();
@@ -20,7 +57,8 @@ const AddDayForm = ({resort, date, powder, backcountry, onNewDay}) => {
     return (
          <form onSubmit={submit} className="add-day-form">
                 <label htmlFor="resort">Resort Name</label>
-                <input id="resort" type="text" required defaultValue={resort} ref={input => _resort = input}/>
+                <Autocomplete options={tahoeResorts} ref={input => _resort = input}/>
+                {/* <input id="resort" type="text" required defaultValue={resort} ref={input => _resort = input}/> */}
 
                 <label htmlFor="date">Date</label>
                 <input id="date" type="date" required defaultValue={date} ref={input => _date = input}/>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Member from './components/Member.jsx';
 import jQuery from 'jquery';
 global.jQuery = jQuery;
 global.$ = jQuery;
@@ -10,7 +11,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'font-awesome/css/font-awesome.min.css';
 
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('app')
+// );
+
 ReactDOM.render(
-    <App />,
+    <Member admin={true}
+            name="Edna Welch"
+            email="edna.welch88@example.com"
+            thumbnail="https://randomuser.me/api/portraits/women/90.jpg"
+            makeAdmin={(email) => console.log(email)}/>,
     document.getElementById('app')
 );
