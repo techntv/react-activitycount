a32fd38ee51ad2796062d5e0b4fea804a6f7bfe7
The Vinh <nguyenthevinh@vienthonga.com>
2017-08-18 10:31:41 +0700

    Update render App

--- src/index.js
+++ src/index.js
@@ -11,16 +11,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'font-awesome/css/font-awesome.min.css';
 
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('app')
-// );
-
 ReactDOM.render(
-    <Member admin={true}
-            name="Edna Welch"
-            email="edna.welch88@example.com"
-            thumbnail="https://randomuser.me/api/portraits/women/90.jpg"
-            makeAdmin={(email) => console.log(email)}/>,
+    <App />,
     document.getElementById('app')
 );
+
